4b1e099cf78c4f81ea52dbb3dc29b0fcc34af027
Conrad Etherington <conrad.etherington@gmail.com>
2022-10-11 23:16:43 -0600

    commit tree cb62bb5eb2128c40bab654a81a77fdf3485dd401

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const headQuarters = 42
+function distanceFromHqInBlocks(location) {
+   // return Math.abs(location - headQuarters)
+    if (headQuarters > location){
+        return headQuarters - location
+    }
+        else if (headQuarters < location){
+            return location - headQuarters
+    }
+}
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+    if  (distanceTravelledInFeet(start, destination) < 400)
+    {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000  ) {
+        let price = (distanceTravelledInFeet(start, destination) -400 )* 0.02 
+        return price
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2500 ) {
+        let price = 25
+        return price
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+}
